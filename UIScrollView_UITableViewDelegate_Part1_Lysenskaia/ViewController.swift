b0d964eb91d1cba0c59726f3f1d83d3ce3ac3d4c
EllenLis <elena139@bk.ru>
2022-10-20 19:58:15 +0300

    Initial Commit

---
+++ UIScrollView_UITableViewDelegate_Part1_Lysenskaia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIScrollView_UITableViewDelegate_Part1_Lysenskaia
+//
+//  Created by ElenaL on 20.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
